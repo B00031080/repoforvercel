a27fa7b6ddc43a99e494c1329fa2ea40b1cc8a5c
B00031080 <b00031080@essec.edu>
2024-03-04 11:27:47 +0100

    Ready for DB

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,6 +19,9 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/tools' ? 'page' : undefined}>
+				<a href="/tools">Tools</a>
+			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
---
+++ src/routes/tools/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data;
+</script>
+
+<h1>List of all Parkside Tools</h1>
+<ul>
+  {#each data.tools as tool}
+    <li>{tool.name} - {tool.category} - {tool.price}</li>
+  {/each}
+</ul>
